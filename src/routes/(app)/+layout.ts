12d3968cb89a79fefa0af3dc6c67dbcd2fa2a27c
Nana Kojo <nkabenyah@gmail.com>
2024-06-10 20:40:05 -0700

    Sixteeth mathlify update commit

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -4,7 +4,7 @@ export const load: LayoutLoad = async ({ depends, data: { user }, fetch }) => {
     depends("appwrite:auth")
     let offline = null
     try {
-        const res = await fetch("http://httpbin.org/get")
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts")
         if (!res.ok) throw Error("No internet")
         offline = false
     } catch {
