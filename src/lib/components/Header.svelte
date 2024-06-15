5f32a7dc445845fdf41cdaf45056901031f26e04
Nana Kojo <nkabenyah@gmail.com>
2024-06-15 14:06:32 -0700

    Logout update commit

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,23 +1,7 @@
 <script lang="ts">
-	import { invalidateAll } from '$app/navigation';
-	import { appwrite } from '$lib/appwrite/client/appwrite';
 	import { onlineStore } from '$lib/stores/onlineStore.svelte';
 	import { type User } from '$lib/types';
 	const { user } = $props<{ user: User | null }>();
-
-	let loggingOut = $state<boolean>(false);
-
-	const logout = async () => {
-		try {
-			loggingOut = true;
-			await fetch("/api/logout", { method: "POST"})
-			invalidateAll();
-		} catch {
-			loggingOut = false
-		} finally {
-			loggingOut = false
-		}
-	};
 </script>
 
 <header class="flex justify-between items-center py-3 px-6">
@@ -26,14 +10,16 @@
 		{#if user === null}
 			<a href="/login" class="py-2 px-4 text-white rounded-lg text-base bg-purple-900">Login</a>
 		{:else}
-			<button
-				onclick={logout}
-				disabled={loggingOut}
-				class="py-2 px-4 flex items-center gap-2 rounded-lg disabled:text-gray-500 text-purple-900 text-lg"
-			>
-				<iconify-icon icon="ic:baseline-logout"></iconify-icon>
-				{loggingOut ? 'Logging out...' : 'Logout'}
-			</button>
+			<form method="post">
+				<button
+					formaction="/?/logout"
+					formmethod="post"
+					class="py-2 px-4 flex items-center gap-2 rounded-lg text-purple-900 text-lg"
+				>
+					<iconify-icon icon="ic:baseline-logout"></iconify-icon>
+					Logout
+				</button>
+			</form>
 		{/if}
 	{/if}
 </header>
