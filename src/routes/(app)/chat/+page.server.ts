11ca24168f3529c4284b6422ffb796a065cbb8d0
Nana Kojo <nkabenyah@gmail.com>
2024-06-23 15:22:31 -0700

    Update

--- src/routes/(app)/chat/+page.server.ts
+++ src/routes/(app)/chat/+page.server.ts
@@ -1,6 +1,6 @@
 export const load = async ({ fetch, depends }) => {
     depends("chat:messages")
-    await fetch("/api/clearUnread")
+    await fetch("/api/clearUnread", { method: "PATCH" })
     const messages = await fetch("/api/getMessages").then(res => res.json());
 
     return { messages }
--- src/routes/api/clearUnread/+server.ts
+++ src/routes/api/clearUnread/+server.ts
@@ -1,8 +1,11 @@
 export const PATCH = async ({ fetch, locals }) => {
+    console.log('clearing unread');
+    
     await fetch(`/api/user/${locals.user?.$id}`, {
         method: "PUT",
         headers: {'Content-type': 'application/json'},
         body: JSON.stringify({ unread_messages: 0 })
     })
+    console.log('done clearing unread');
     return new Response("Success")
 }
