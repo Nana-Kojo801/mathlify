f30dfe8712301784b87e752f5b89779c1932d59b
Nana Kojo <nkabenyah@gmail.com>
2025-06-29 13:12:08 -0700

    Enhanced match-found ui

--- src/routes/app/online/match/-components/rapid/rapid.tsx
+++ src/routes/app/online/match/-components/rapid/rapid.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import FindRapidMatch from './find-rapid-match'
 import Play from './play'
+import { useActiveRapidMatch } from '../hooks'
 
 const Rapid = () => {
   const [rapidState, setRapidState] = useState<'searching' | 'playing'>(
     'searching',
   )
+  const match = useActiveRapidMatch()
+  const isInMatch = useMemo(() => (match && match.status !== "result") ?? false, [match])
 
   return (
     <>
-      {rapidState === 'searching' && (
+      {(rapidState === 'searching' && !isInMatch) && (
         <FindRapidMatch setRapidState={setRapidState} />
       )}
-      {rapidState === 'playing' && <Play setRapidState={setRapidState} />}
+      {(rapidState === 'playing' || isInMatch) && <Play setRapidState={setRapidState} />}
     </>
   )
 }
