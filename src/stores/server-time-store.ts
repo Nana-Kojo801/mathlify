fd70390b45f039188a5c8e00310e3398debcbfbb
Nana Kojo <nkabenyah@gmail.com>
2025-06-29 00:53:53 -0700

    Fix server time fetch to correctly parse response as text

--- src/stores/server-time-store.ts
+++ src/stores/server-time-store.ts
@@ -74,7 +74,7 @@ const estimateServerTime = (serverTime: number, latency: number): number => {
 const performTimeSync = async (convex: ReturnType<typeof useConvex>): Promise<SyncResult> => {
   try {
     const requestTime = Date.now()
-    const tempServerTime = await fetch('/.netlify/functions/server-time').then(res => Number(res))
+    const tempServerTime = Number(await fetch('/.netlify/functions/server-time').then(res => res.text()))
     console.log('[SERVER_TIME_SYNC]: Temporary server time fetched', tempServerTime);
     
     const serverTime = await convex.query(api.serverTime.get)
